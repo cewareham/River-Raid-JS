d29a5752ca06e70394f5036cac44c279aec561a1
cewareham <cwareham007@gmail.com>
2020-04-19 00:38:47 -0600

    Added Shape class-plane, heli, trees, etc

--- js/game.js
+++ js/game.js
@@ -4,7 +4,17 @@
 class Game {
     constructor(canvas) {
         this.canvas = canvas;
+        this.game = false;
+        this.goout = false;
+        // define shapes - easier than using #s
+        this.eShape = {
+            PLANE: 0, PLANE_LEFT: 1, PLANE_RIGHT: 2, HELI_RIGHT0: 3, HELI_RIGHT1: 4,
+            HELI_LEFT0: 5, HELI_LEFT1: 6, SHIP_RIGHT: 7, SHIP_LEFT: 8, AIRPLANE_E: 9,
+            AIRPLANE_D: 10, FUEL: 11, EXPL1:12, EXPL2: 13, HOME: 14
+        }
         this.centerCanvas();
+        //                      x   y width height  shape#        out expl
+        this.plane = new Shape(370, 420, 49, 42, this.eShape.PLANE, 0, 0);
     }
   
     update() {
@@ -14,6 +24,8 @@ class Game {
     render() {
         // river
         background(clr[3]);     // river -> blue
+
+        this.plane.show();
     }
 
     centerCanvas() {
